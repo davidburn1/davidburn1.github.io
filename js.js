b1f21c633ae88566934e567d00247bdabe739324
davidburn1 <56227829+davidburn1@users.noreply.github.com>
2020-04-19 11:29:14 +0100

    og meta update

--- js.js
+++ js.js
@@ -2,29 +2,56 @@ var app = angular.module('app', ['ui.router', 'ngSanitize']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
+    $stateProvider.state("/", {
+        url: "/",
+        templateUrl: "home.html",
+        onEnter: function($rootScope){
+            $rootScope.pageTitle = "David Burn - Magnetism";
+            $rootScope.og.title = "David Burn - Magnetism";
+            $rootScope.og.image = "";
+            $rootScope.og.description = "";
+            $rootScope.og.url = "https://davidburn1.github.io";
+        },
+    });
+
     $stateProvider.state("/publications", {
         url: "/publications",
         templateUrl: "papers.html",
+        onEnter: function($rootScope){
+            $rootScope.pageTitle = "David Burn - Publications";
+            $rootScope.og.title = "David Burn - Publications";
+            $rootScope.og.image = "https://davidburn1.github.io/images/li_publications.jpg";
+            $rootScope.og.description = "Publications in peer-reviewed international journals";
+            $rootScope.og.url = "https://davidburn1.github.io/#!/publications";
+        },
     });
+
 
     $stateProvider.state("/publications/{key}", {
         url: "/publications/{key}",
         templateUrl: "papers.html",
     });
 
-    $stateProvider.state("/", {
-        url: "/",
-        templateUrl: "home.html",
-    });
+
 
     $urlRouterProvider.otherwise('/');
 });
 
 
 
+app.run(['$rootScope', function ($rootScope) {
+    $rootScope.pageTitle = "David Burn - magnetism";
+
+    $rootScope.og = {};
+    $rootScope.og.title = "David Burn - Magnetism";
+    $rootScope.og.image = "";
+    $rootScope.og.description = "";
+    $rootScope.og.url = "https://davidburn1.github.io";
+ }]);
 
 
-app.controller('papersController', function($scope, $http) {
+
+app.controller('papersController', function($scope, $http, $rootScope) {
     $scope.papers = [];
 
     $http.get("papers.json")
@@ -37,8 +64,6 @@ app.controller('papersController', function($scope, $http) {
         console.log(authors);
         
     }
-
-
 });
 
 
