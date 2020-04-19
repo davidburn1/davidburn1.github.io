d24d3439d6a77d3c3c01f6fb21ab08d62e147431
davidburn1 <56227829+davidburn1@users.noreply.github.com>
2020-04-19 19:52:02 +0100

    add presentations

--- js.js
+++ js.js
@@ -18,6 +18,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
         },
     });
 
+    $stateProvider.state("/presentations", {
+        url: "/presentations",
+        templateUrl: "presentations.html",
+        onEnter: function($rootScope){
+            $rootScope.pageTitle = "David Burn - Presentations";
+        },
+    });
 
     $stateProvider.state("/publications/{key}", {
         url: "/publications/{key}",
@@ -44,18 +51,39 @@ app.controller('papersController', function($scope, $http, $rootScope) {
     .then(function(response) {
         $scope.papers = response.data;
     });
+});
 
-    $scope.formatAuthors = function(authors){
-        console.log("fa");
-        console.log(authors);
-        
+app.controller('presentationsController', function($scope, $http, $rootScope) {
+    $scope.presentations = [];
+
+    $http.get("presentations.json")
+    .then(function(response) {
+        $scope.presentations = response.data;               
+    });
+
+    $scope.selected = {};
+    $scope.getAbstract = function(key){
+        $scope.selected = {};
+        for (var i=0; i<$scope.presentations.length; i++) {
+            if ($scope.presentations[i].key == key){
+                $scope.selected = $scope.presentations[i];
+            }
+        }
+        $scope.selected.abstract = "";
+
+        $http.get("abstracts/presentations/"+key+".html")
+        .then(function(response) {
+            $scope.selectedAbstract = response.data;             
+        },
+        function(data) {
+            $scope.selectedAbstract = "no abstract";
+        });
     }
 });
 
-
-
 app.filter('formatAuthors', function() {
     return function(authors) {
+        if (authors === undefined) return ""
         return authors.replace("D.M. Burn", "<b>D.M. Burn</b>");
     };
   });
@@ -75,3 +103,49 @@ app.directive("mathjaxBind", function() {
             }]
     };
 });
+
+
+
+
+app.directive('flickitySlide', ['$injector', function($injector){
+    return {
+        restrict : 'E',
+        template : '<div ng-transclude></div>',
+        transclude : true,
+        link: function($scope, $element, attributes){
+        	var lastTypeof = typeof($scope.$last), reInit = function(){
+            var holder = $element.parent()[0], flickity = Flickity.data(holder);
+           	flickity && flickity.destroy();
+           	new Flickity(holder, angular.fromJson(holder.getAttribute('options'))); //data-flickity="..." does not work for me
+          };
+        	if($scope.$last || (lastTypeof === 'undefined' && !$element[0].nextElementSibling) || (lastTypeof !== 'undefined' && Flickity.data($element.parent()[0]))){
+          	reInit();
+          }
+        }
+    };
+}])
+
+
+
+
+
+
+
+// $carousel.on('staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
+//     if ( !cellElement ) { return; }   // dismiss if cell was not clicked
+//     console.log( 'Flickity clicked at ' + cellIndex)
+//     $carousel.flickity( 'selectCell', cellIndex );
+//     ga('send','event','pres_slider','clicked','', cellElement)
+// });
+
+// $carousel.on('select.flickity', function() {
+//     pres = presentations[flkty.selectedIndex]
+//     $('#presTitle').html(pres.title)
+//     $('#presConference').html(pres.conference + " - " + pres.date)
+//     ga('send','event','pres_slider','selected',pres.key, pres.title)
+// })
+
+// $('#btn_view_pres').click(function() {
+//     pres = presentations[flkty.selectedIndex]
+//     document.location.href="/presentations#" + pres.key;
+// });
