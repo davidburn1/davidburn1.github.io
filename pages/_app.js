d524b620bbb294051be1f54ddb7d6c27ce5527d1
davidburn1 <56227829+davidburn1@users.noreply.github.com>
2021-03-22 11:25:25 +0000

    ga update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/Layout';
 import Router from 'next/router'
-// import { Fragment } from 'react'
-
-import GoogleAnalytics from "../components/googleAnalytics.js"
 
 // add bootstrap css 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,13 +8,19 @@ import "../public/styles.css";
 
 
 
-// Notice how we track pageview when route is changed
-// Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 
 
 
 export default function MyApp({ Component, pageProps }) {
+
+  // Notice how we track pageview when route is changed
+  Router.events.on('routeChangeComplete', function(url){
+    gtag('config', 'UA-163960416-1', {page_path:url});
+  })
+
+
+
   return (
 <div>
     <Head>
@@ -28,22 +31,21 @@ export default function MyApp({ Component, pageProps }) {
     <meta name="description" content="David Burn - Research scientist working on nanoscale magnetic devices. Interested in dynamic magnetisation processes, spintronic and electronic behaviour and nanotechnology device fabrication."/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-    {/* <Fragment>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163960416-1"/>
-    <script dangerouslySetInnerHTML={{
-      __html: "window.dataLayer = window.dataLayer || []; \
-              function gtag(){dataLayer.push(arguments)}\
-              gtag('js', new Date());"
-    }}/>
-    </Fragment> */}
-
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-163960416-1' />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-163960416-1', {page_path: window.location.pathname,});`,
+      }}
+    />
  
 
   </Head>
-  <GoogleAnalytics />
-
-    <Layout>
+     <Layout>
     <Component {...pageProps} />
     </Layout>
 
